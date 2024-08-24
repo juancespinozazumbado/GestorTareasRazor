2aca01c8c66a64bb34a2eeb7bf85066688af532f
juancespinozazumbado <espinozajuanki@gmail.com>
2024-08-24 13:24:26 -0600

    Agrega migracion Tareas

--- src/GestorDeTareas.Web/Data/Migrations/20240824192131_TareasMigration.Designer.cs
+++ src/GestorDeTareas.Web/Data/Migrations/20240824192131_TareasMigration.Designer.cs
@@ -4,6 +4,7 @@ using GestorDeTareas.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestorDeTareas.Web.Data.Migrations
 {
     [DbContext(typeof(TareasContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240824192131_TareasMigration")]
+    partial class TareasMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
