f62fece91573890da905ba8bb8a46926f3b8d964
juancespinozazumbado <espinozajuanki@gmail.com>
2024-08-25 11:51:12 -0600

    corrige bug Database recreate on restart

--- src/GestorDeTareas.Web/Data/Dependencias.cs
+++ src/GestorDeTareas.Web/Data/Dependencias.cs
@@ -4,16 +4,25 @@ namespace GestorDeTareas.Web.Data;
 
 public static class Dependencias
 {
-    public static IServiceCollection AgregarDbContext(this IServiceCollection services, IConfiguration configuration, bool enMemory = false)
+    public static IServiceCollection AgregarDbContext(this IServiceCollection services, IConfiguration configuration, bool enMemory = false, bool useSqlite = false)
     {
         if (enMemory)
+        {
+
+
+            services.AddDbContext<TareasContext>(options =>
+                options.UseInMemoryDatabase("Tareasdb"));
+
+
+        }
+        if (useSqlite)
         {
             var connectionString = Environment.GetEnvironmentVariable("SQLITECONNECTIONSTRING", EnvironmentVariableTarget.User);
 
             services.AddDbContext<TareasContext>(options =>
                 options.UseSqlite(connectionString));
-
         }
+
         else
         {
             var connectionString = Environment.GetEnvironmentVariable("SQLSERVERCONNECTION");
